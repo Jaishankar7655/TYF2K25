ada7450e4e1cdfb3452a3f55b82c199637d474b4
jaishankar prasad jaiswal <jaishankar7655@gmail.com>
2025-02-26 09:13:27 +0530

    chnages in the spelling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
       <p className="text-center my-6" >© 2025 Truba Youth Fest. All rights reserved.</p>
-      <p className="text-center text-slate-600" >Created and developed By student Committe of Truba group of Institute</p>
+      <p className="text-center text-slate-600" >Created and developed By student Committee (SOC) of Truba group of Institute</p>
     </footer>
   );
 }
