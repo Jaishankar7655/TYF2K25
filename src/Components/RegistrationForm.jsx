1c67ec025c3bb11d9195d9c6b119d400268ec70a
jaishankar7655 <Jaishankar7655@gmail.com>
2026-04-01 11:21:30 +0530

    Remove 'Teacher's Ramp Walk' from events list
    Removed 'Teacher's Ramp Walk' event from the registration options.

--- src/Components/RegistrationForm.jsx
+++ src/Components/RegistrationForm.jsx
@@ -102,7 +102,7 @@ const categories = [
       { name: "Poetry", price: 0, closingDate: "2026-04-07T23:59:00+05:30", closingDateStr: "7 APRIL 2026" },
       { name: "Bottle Flip", price: 0, closingDate: "2026-04-07T23:59:00+05:30", closingDateStr: "7 APRIL 2026" },
       { name: "Thug of War (Per Team, Max 10)", price: 300, closingDate: "2026-04-07T23:59:00+05:30", closingDateStr: "7 APRIL 2026" },
-      { name: "Teacher's Ramp Walk", price: 0, closingDate: "2026-04-07T23:59:00+05:30", closingDateStr: "7 APRIL 2026" },
+      
       { name: "Dance-Freeze Challenge", price: 0, closingDate: "2026-04-07T23:59:00+05:30", closingDateStr: "7 APRIL 2026" },
       { name: "Paper Folding Dance", price: 0, closingDate: "2026-04-07T23:59:00+05:30", closingDateStr: "7 APRIL 2026" },
       { name: "Truba Roadies", price: 100, closingDate: "2026-04-07T23:59:00+05:30", closingDateStr: "7 APRIL 2026" },
